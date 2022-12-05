d34e2cd010fc8ae5b8eabef70f0aeaec3d304519
Dimitris Kriezis <dkriezis@mit.edu>
2022-12-04 22:40:14 -0500

    Propogate deletion for Stroll and Certified Residency

--- server/neighborhood/router.ts
+++ server/neighborhood/router.ts
@@ -2,6 +2,8 @@ import type { Request, Response } from 'express';
 import express from 'express';
 import NeighborhoodCollection from './collection';
 import ReviewCollection from '../review/collection';
+import StrollCollection from '../stroll/collection';
+import CertifiedResidencyCollection from '../certifiedResidency/collection';
 import * as neighborhoodValidator from './middleware';
 import * as userValidator from '../user/middleware';
 import * as util from './util';
@@ -203,6 +205,8 @@ router.delete(
 
     await NeighborhoodCollection.deleteOneByInfo(name, city, state);
     await ReviewCollection.deleteManyByLocation(neighborhoodId)
+    await CertifiedResidencyCollection.deleteManyByLocation(neighborhoodId);
+    await StrollCollection.deleteManyByLocation(neighborhoodId);
     res.status(200).json({
       message: `Neighborhood ${util.formatWord(name)}, ${util.formatWord(city)}, ${state.toUpperCase()} has been deleted successfully.`
     });
