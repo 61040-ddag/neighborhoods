9b7284cca03bceae197fad95e04dbabf3b6255c9
Deniz Sert <dsert@mit.edu>
2022-12-10 15:47:53 -0500

    styled homepage

--- client/components/Home/HomePage.vue
+++ client/components/Home/HomePage.vue
@@ -1,30 +1,34 @@
 <template>
-    <main>
-        <section v-if="$store.state.username">
-            <header>
-                <h2>Welcome @{{ $store.state.username }}!</h2>
-            </header>
-            <GetNeighborhoodsForm
-                neighborhoodPlaceholder="Opt: Enter neighborhood"
-                cityPlaceholder="Req: Enter city. Ex: Boston"
-                statePlaceholder="Req: Enter state. Ex: MA, NY"
-                button="🔄 Get neighborhood(s)"
-            />
-        </section>
-        <section v-else>
-            <header>
-                <h2>Welcome to Neighborhoods!</h2>                
-            </header>
-            <article>
-                <h3>
-                    <router-link to="/login" class="routerLink">
-                        Log in
-                    </router-link>
-                    to find your dream neighborhood and start a family!
-                </h3>
-            </article>
-        </section>
-    </main>
+    <div class="outermost-container">
+        <main>
+            <div class="container outer-container"> 
+                <div v-if="$store.state.username" class="header text-center">
+                    <header>
+                        <h2 class="big-text">Welcome @{{ $store.state.username }}!</h2>
+                    </header>
+                    <GetNeighborhoodsForm
+                        neighborhoodPlaceholder="Opt: Enter neighborhood"
+                        cityPlaceholder="Req: Enter city. Ex: Boston"
+                        statePlaceholder="Req: Enter state. Ex: MA, NY"
+                        button="🔄 Get neighborhood(s)"
+                    />
+                </div>
+                <div v-else>
+                    <header>
+                        <h2>Welcome to Neighborhoods!</h2>                
+                    </header>
+                    <article>
+                        <h3>
+                            <router-link to="/login" class="routerLink">
+                                Log in
+                            </router-link>
+                            to find your dream neighborhood and start a family!
+                        </h3>
+                    </article>
+                </div>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
@@ -39,7 +43,29 @@ export default {
 </script>
 
 <style scoped>
+.header {
+    margin: auto;
+    text-align: center;
+    display: inline;
+    margin: auto;
+    width: 60%;
+    padding: 10px;
+    align-items: center;
+}
+
+.outer-container {
+}
+
+.outermost-container {
+    padding-top: 20%;
+    border: 3px solid blue;
+}
+
 .routerLink {
     text-decoration: none;
+}
+
+.big-text {
+    font-size: 50px;
 }
 </style>
